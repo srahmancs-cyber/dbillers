2738409c51580261e07290ea07fa9c0d91c67a84
srahmancs-cyber <shahid@mooshdigital.com>
2026-05-14 04:45:48 +0500

    Update favicon: add SVG, 96x96 PNG, remove old 16x16/32x32 references

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,11 +27,10 @@
     <link rel="canonical" href="@yield('canonical', url()->current())">
     
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon/favicon.ico') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon/favicon.svg') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/favicon-96x96.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
-    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon/favicon.ico') }}">
     
     <!-- AOS CSS -->
     <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
